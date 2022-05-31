87b246baae13342802afcccc67c62bce3710460b
Marcellus Nwankwo <cellusnwankwo@gmail.com>
2022-05-31 17:37:10 +0100

    404

---
+++ pages/404.js
@@ -0,0 +1,34 @@
+import Link from 'next/link'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+
+
+export default function NotFound() {
+    const router = useRouter()
+
+    useEffect(() => {
+        setTimeout(() => {
+            router.push('/')
+        }, 3000)
+    }, [])
+
+  return (
+    <div>
+        <h1>404</h1>
+        <h2>Ooops! That page cannot be found :(</h2>
+        <p>Reirecting to the <Link href="/">Homepage</Link> for more sensational goodness...</p>
+
+        <style jsx>{`
+        .not-found{
+            background:#fff;
+            padding: 30px;
+            box-shadow: 1px 3px 5px rgba(0,0,0,0.1);
+            transform: rotatez(-1deg);
+        }
+        h1{
+            font-size: 3em;
+        }
+        `}</style>
+    </div>
+  )
+}
